9fdb4dbc40279da03ec95a77c1b54f561de4098a
Kira Yagami <52878074+vutung1991@users.noreply.github.com>
2023-06-27 10:58:45 -0400

    Initial commit

---
+++ src/main/java/com/revature/repositories/UserDAO.java
@@ -0,0 +1,26 @@
+package com.revature.repositories;
+
+import com.revature.models.User;
+
+import java.util.Optional;
+
+public class UserDAO {
+
+    /**
+     * Should retrieve a User from the DB with the corresponding username or an empty optional if there is no match.
+     */
+    public Optional<User> getByUsername(String username) {
+        return Optional.empty();
+    }
+
+    /**
+     * <ul>
+     *     <li>Should Insert a new User record into the DB with the provided information.</li>
+     *     <li>Should throw an exception if the creation is unsuccessful.</li>
+     *     <li>Should return a User object with an updated ID.</li>
+     * </ul>
+     */
+    public User create(User userToBeRegistered) {
+        return userToBeRegistered;
+    }
+}
